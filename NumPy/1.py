81266f7aaf69946547ba13086114e7eed5568408
iskandarkholmatov <iskandar.kholmatov@wmich.edu>
2023-02-28 10:44:38 -0500

    numpy

---
+++ NumPy/1.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+a = np.arange(10) 
+print("The ndarray is :")
+print(a)
+
+s = slice(2,7,2) 
+print("After applying slice() Function:")
+print (a[s])
---
+++ NumPy/lecture-1.py
@@ -0,0 +1,4 @@
+import numpy as np 
+
+a = np.arange([1,2,3],[4,6,6], dtype=np.float32)
+print(a.ndim, a.shape, a.dtype)
